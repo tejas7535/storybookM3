a193d4afe88b6d1462f71e23f70116638028ded4
Stefan Herpich <herpisef@schaeffler.com>
2020-01-16 09:48:32 +0100

    feat(sta): prod environment for sta (UFTABI-1463)

--- apps/sta/src/environments/environment.prod.ts
+++ apps/sta/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiBaseUrl: '/api/v1',
   azureTenantId: '67416604-6509-4014-9859-45e709f53d3f',
-  azureClientId: 'dd7cb687-82e3-485d-9b26-d202662ee2ee',
-  azureBackendAPI: 'api://b41af2bd-a240-46fe-82a0-e3e9e11f945a/staapi'
+  azureClientId: '67416604-6509-4014-9859-45e709f53d3f',
+  azureBackendAPI: 'api://e948e642-7a37-4ca5-b2c1-ba1eb18c97f5/staapi'
 };
