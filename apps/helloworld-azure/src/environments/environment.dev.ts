cc5735bb1a7d40224d7179c67c8a44bae3cbf4df
Stefan Herpich <herpisef@schaeffler.com>
2020-02-19 08:54:58 +0100

    fix(helloworld-azure): fix basePaths in environments

--- apps/helloworld-azure/src/environments/environment.dev.ts
+++ apps/helloworld-azure/src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiBaseUrl: 'https://hello-world-d.dev.dp.schaeffler',
+  apiBaseUrl: '',
   production: false,
   azureTenantId: '67416604-6509-4014-9859-45e709f53d3f',
   azureClientId: '7421bc7b-30e5-4660-aac6-fb9382b3c365',
