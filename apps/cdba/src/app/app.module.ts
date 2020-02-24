7dc8cdad3170ee907ee4df3a455add69cb559806
Stefan Herpich <herpisef@schaeffler.com>
2020-02-24 08:21:37 +0100

    feat(cdba): add translation functionality (DSCDA-1305)

--- apps/cdba/src/app/app.module.ts
+++ apps/cdba/src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { SharedTranslocoModule } from '@schaeffler/shared/transloco';
 import {
   HeaderModule,
   SettingsSidebarModule
@@ -10,12 +12,22 @@ import {
 
 import { AppComponent } from './app.component';
 
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserAnimationsModule,
     FlexLayoutModule,
     HeaderModule,
+    HttpClientModule,
+    SharedTranslocoModule.forRoot(
+      environment.production,
+      ['en'],
+      'en', // default -> undefined would lead to browser detection
+      'en',
+      true
+    ),
     SettingsSidebarModule,
     MatButtonModule
   ],
