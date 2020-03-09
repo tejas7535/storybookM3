f4a785ae5f7259728b6b705e91b346fbc5aebcf6
Thomas Birke <birketho@schaeffler.com>
2020-03-09 14:27:39 +0100

    feat(sta): initial store setup (UFTABI-2124)

--- apps/sta/src/environments/environment.ts
+++ apps/sta/src/environments/environment.ts
@@ -3,5 +3,6 @@ export const environment = {
   apiBaseUrl: 'https://dev.sta.dp.schaeffler/api/v1',
   azureTenantId: '67416604-6509-4014-9859-45e709f53d3f',
   azureClientId: 'dd7cb687-82e3-485d-9b26-d202662ee2ee',
-  azureBackendAPI: 'api://b41af2bd-a240-46fe-82a0-e3e9e11f945a/staapi'
+  azureBackendAPI: 'api://b41af2bd-a240-46fe-82a0-e3e9e11f945a/staapi',
+  devToolsEnabled: true
 };
