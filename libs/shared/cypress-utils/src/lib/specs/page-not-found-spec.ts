2d42625ea3cbd42e8a124fa3ebdc4b070e5360e9
Fabian Kaupp <kauppfbi@schaeffler.com>
2020-03-10 08:33:57 +0100

    refactor(cypress-utils): apply selecting best practice for 404 spec

--- libs/shared/cypress-utils/src/lib/specs/page-not-found-spec.ts
+++ libs/shared/cypress-utils/src/lib/specs/page-not-found-spec.ts
@@ -5,7 +5,7 @@ export const testPageNotFound = (
   it('should route to 404 Page and return back to home', () => {
     cy.visit(nonExistentUrl);
 
-    cy.get('#pageNotFound')
+    cy.get('[data-cy="pageNotFound"]')
       .should('be.visible')
       .get('[data-cy="visitHome"]')
       .click()
