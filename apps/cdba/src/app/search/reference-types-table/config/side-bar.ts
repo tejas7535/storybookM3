9ff48a0f9835c3aa766988c81c988948ef26e55d
Fabian Kaupp <kauppfbi@schaeffler.com>
2020-05-13 08:39:26 +0200

    feat(cdba): add reference types table with ag grid (DSCDA-1457)

---
+++ apps/cdba/src/app/search/reference-types-table/config/side-bar.ts
@@ -0,0 +1,26 @@
+import { SideBarDef } from '@ag-grid-enterprise/all-modules';
+
+export const SIDE_BAR_CONFIG: SideBarDef = {
+  toolPanels: [
+    {
+      id: 'columns',
+      labelDefault: 'Columns',
+      labelKey: 'columns',
+      iconKey: 'columns',
+      toolPanel: 'agColumnsToolPanel',
+      toolPanelParams: {
+        suppressValues: true,
+        suppressPivots: true,
+        suppressPivotMode: true,
+      },
+    },
+    {
+      id: 'filters',
+      labelDefault: 'Filters',
+      labelKey: 'filters',
+      iconKey: 'filter',
+      toolPanel: 'agFiltersToolPanel',
+    },
+  ],
+  defaultToolPanel: 'columns',
+};
--- apps/cdba/src/app/search/reference-types-table/reference-types-table.component.spec.ts
+++ apps/cdba/src/app/search/reference-types-table/reference-types-table.component.spec.ts
@@ -1,9 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatIconModule } from '@angular/material/icon';
 
+import { AgGridModule } from '@ag-grid-community/angular';
 import { configureTestSuite } from 'ng-bullet';
+
+import {
+  provideTranslocoTestingModule,
+  SharedTranslocoModule,
+} from '@schaeffler/transloco';
 
 import { SharedModule } from '../../shared/shared.module';
 import { ReferenceTypesTableComponent } from './reference-types-table.component';
+import { DetailViewButtonComponent } from './status-bar/detail-view-button/detail-view-button.component';
+
+jest.mock('@ngneat/transloco', () => ({
+  ...jest.requireActual('@ngneat/transloco'),
+  translate: jest.fn(() => 'translate it'),
+}));
 
 describe('ReferenceTypesTableComponent', () => {
   let component: ReferenceTypesTableComponent;
@@ -11,8 +24,15 @@ describe('ReferenceTypesTableComponent', () => {
 
   configureTestSuite(() => {
     TestBed.configureTestingModule({
-      imports: [SharedModule],
-      declarations: [ReferenceTypesTableComponent],
+      imports: [
+        SharedTranslocoModule,
+        SharedModule,
+        AgGridModule.withComponents([DetailViewButtonComponent]),
+        MatIconModule,
+        provideTranslocoTestingModule({}),
+      ],
+      declarations: [ReferenceTypesTableComponent, DetailViewButtonComponent],
+      providers: [],
     });
   });
 
