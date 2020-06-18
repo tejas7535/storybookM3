25d8a3646ab6db0fad19fcacad0bec61d1f6a26a
Robert Krause <krausrbe@schaeffler.com>
2020-06-18 13:32:07 +0200

    feat(footer): add storybook stories (UFTABI-2513)

--- libs/shared/ui/footer/.storybook/config.js
+++ libs/shared/ui/footer/.storybook/config.js
@@ -1,5 +1,8 @@
-import { configure, addDecorator } from '@storybook/angular';
+import { withA11y } from '@storybook/addon-a11y';
 import { withKnobs } from '@storybook/addon-knobs';
+import { addDecorator, configure } from '@storybook/angular';
 
 addDecorator(withKnobs);
+addDecorator(withA11y);
+
 configure(require.context('../src/lib', true, /\.stories\.tsx?$/), module);
