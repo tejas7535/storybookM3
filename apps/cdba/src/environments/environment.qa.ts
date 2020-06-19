37d12d07828ab057d79a74b1900a5e938be64e13
Fabian Kaupp <kauppfbi@schaeffler.com>
2020-06-19 10:19:19 +0200

    fix(cdba): fix aad credentials for dev

--- apps/cdba/src/environments/environment.qa.ts
+++ apps/cdba/src/environments/environment.qa.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   devToolsEnabled: true,
   tenantId: '67416604-6509-4014-9859-45e709f53d3f',
-  clientId: 'd8bf880a-2d95-4358-a846-b2ca465c9e06',
-  appId: 'api://2d05baed-6678-4607-a84e-42301ff9f56f/cdbaapi',
+  clientId: '9b41cee5-3a94-45cb-b36f-35d748d9c0bf', // SG_CDBA_Q
+  appId: 'api://653310f5-aeab-47f6-ae89-69f1211ffee3/cdbaapi', // SG_CDBA_Q_API
   baseUrl: '/api/v1',
 };
