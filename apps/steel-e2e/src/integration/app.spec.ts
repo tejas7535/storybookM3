c780b947c3e2064e8f9f00bb787ccf93afc271f7
Christian Berndt <berndcri@schaeffler.com>
2020-07-07 14:31:09 +0200

    feat(steel): remove steel from workspace (UFTABI-2494)

--- apps/steel-e2e/src/integration/app.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('steel', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('should display welcome message', () => {
-    cy.get('h3').contains('STEEL');
-  });
-});
--- apps/steel/src/app/features/extensions/extensions.module.ts
+++
@@ -1,9 +0,0 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-
-import { ExtensionsRoutingModule } from './extensions-routing.module';
-
-@NgModule({
-  imports: [CommonModule, ExtensionsRoutingModule]
-})
-export class ExtensionsModule {}
