f52ddf6ca7efeca9025b573c160f4674f09d95a2
Fabian Kaupp <kauppfbi@schaeffler.com>
2020-08-10 13:57:10 +0200

    fix(storybook): fix translation issues of empty states (UFTABI-2747)

--- libs/shared/ui/storybook/src/lib/forbidden.component.stories.ts
+++ libs/shared/ui/storybook/src/lib/forbidden.component.stories.ts
@@ -2,11 +2,23 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { ForbiddenComponent, ForbiddenModule } from '@schaeffler/empty-states';
+import { SharedTranslocoModule } from '@schaeffler/transloco';
 
 import READMEMd from '../../../empty-states/src/lib/forbidden/README.md';
 
 const moduleMetadata = {
-  imports: [ForbiddenModule, HttpClientModule, RouterTestingModule],
+  imports: [
+    ForbiddenModule,
+    HttpClientModule,
+    RouterTestingModule,
+    SharedTranslocoModule.forRoot(
+      false,
+      ['en'],
+      'en', // default -> undefined would lead to browser detection
+      'en',
+      false
+    ),
+  ],
 };
 
 const baseComponent = {
