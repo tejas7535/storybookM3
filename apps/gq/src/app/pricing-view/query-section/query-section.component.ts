b3f6c68ca518d83c3eb91618aeb50dad158c3aff
Stefan Herpich <herpisef@schaeffler.com>
2020-09-11 09:58:30 +0200

    feat(gq): migrate query section (GQUOTE-109)

--- apps/gq/src/app/pricing-view/query-section/query-section.component.ts
+++ apps/gq/src/app/pricing-view/query-section/query-section.component.ts
@@ -3,5 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'gq-query-section',
   templateUrl: './query-section.component.html',
+  styleUrls: ['./query-section.component.scss'],
 })
-export class QuerySectionComponent {}
+export class QuerySectionComponent {
+  /**
+   * Improves performance of ngFor.
+   */
+  public trackByFn(_index: number, item: number): number {
+    return item;
+  }
+}
