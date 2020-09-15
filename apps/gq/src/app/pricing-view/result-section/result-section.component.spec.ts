473275fa48ceddce7a259c4c6f1e01efcd8af8e3
Stefan Herpich <herpisef@schaeffler.com>
2020-09-15 07:08:45 +0200

    feat(gq): migrate result section (GQUOTE-110)

--- apps/gq/src/app/pricing-view/result-section/result-section.component.spec.ts
+++ apps/gq/src/app/pricing-view/result-section/result-section.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { AgGridModule } from '@ag-grid-community/angular';
 import { configureTestSuite } from 'ng-bullet';
 
 import { ResultSectionComponent } from './result-section.component';
@@ -11,6 +12,7 @@ describe('resultComponent', () => {
   configureTestSuite(() => {
     TestBed.configureTestingModule({
       declarations: [ResultSectionComponent],
+      imports: [AgGridModule.withComponents([])],
     });
   });
 
--- apps/gq/src/app/pricing-view/result-section/result-section.module.ts
+++ apps/gq/src/app/pricing-view/result-section/result-section.module.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { AgGridModule } from '@ag-grid-community/angular';
 
 import { ResultSectionComponent } from './result-section.component';
 
 @NgModule({
   declarations: [ResultSectionComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, AgGridModule.withComponents([]), FlexLayoutModule],
   exports: [ResultSectionComponent],
 })
 export class ResultSectionModule {}
