40bfd94539b1b79a118da4a19e26fdb0dc4c8200
Sir Henry <a1173595@schaeffler.com>
2020-11-17 15:32:28 +0100

    chore(deps): update dependency jwt-decode to v3.1.2

--- apps/ltp/src/app/shared/models/access-token.model.ts
+++ apps/ltp/src/app/shared/models/access-token.model.ts
@@ -1,24 +1,20 @@
-export interface AccessToken {
+import { JwtPayload } from 'jwt-decode';
+
+export interface AccessToken extends JwtPayload {
   acr: string;
   aio: string;
   amr: string[];
   appid: string;
   appidacr: string;
-  aud: string;
-  exp: number;
   family_name: string;
   given_name: string;
-  iat: number;
   ipaddr: string;
-  iss: string;
   name: string;
-  nbf: number;
   oid: string;
   onprem_sid: string;
   rh: string;
   roles: string[];
   scp: string;
-  sub: string;
   tid: string;
   unique_name: string;
   upn: string;
