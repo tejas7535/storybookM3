d2c6ac128019ccc1ca73466efb2e60187840c152
herpisef <herpisef@schaeffler.com>
2020-11-20 11:28:18 +0100

    feat(ia): add application insights (IA-63)

--- apps/ia/src/environments/environment.prod.ts
+++ apps/ia/src/environments/environment.prod.ts
@@ -9,4 +9,16 @@ export const environment = {
   tenantId: '67416604-6509-4014-9859-45e709f53d3f',
   clientId: '1345d78e-c59f-4cf3-9086-69953e8dc995',
   appId: 'api://54579610-09f1-48ab-83d7-ecfca5ab7436/iaapi',
+
+  // Application Insights
+  applicationInsights: {
+    applicationInsightsConfig: {
+      instrumentationKey: '5b74a960-030c-4dfc-a267-be3accb26a9d',
+      isCookieUseDisabled: true,
+      autoTrackPageVisitTime: true,
+    },
+    enableGlobalErrorHandler: true,
+    enableNgrxMetaReducer: true,
+    ngrxIgnorePattern: ['@ngrx/*'],
+  },
 };
