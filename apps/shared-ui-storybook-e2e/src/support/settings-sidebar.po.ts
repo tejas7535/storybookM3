84ffb8c9da35bb326d47fb7304168b02fde1d536
krausrbe <krausrbe@schaeffler.com>
2021-04-14 15:32:10 +0200

    chore(storybooks): e2e fix attempt

--- apps/shared-ui-storybook-e2e/src/support/settings-sidebar.po.ts
+++ apps/shared-ui-storybook-e2e/src/support/settings-sidebar.po.ts
@@ -1,7 +1,8 @@
 export const testSettingsSidebar = () => {
   cy.visit('/iframe.html?id=settings-sidebar--primary');
 
-  cy.get('[data-cy=close-settings-sidebar]')
+  cy.window()
+    .get('[data-cy=close-settings-sidebar]')
     .click()
     .get('[data-cy=open-settings-sidebar]')
     .click()
