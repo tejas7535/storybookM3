6c17d2a139f0da13ea67eaf0292b7b6b2a9c9503
Philip Riecks <mail@philipriecks.de>
2021-05-04 10:26:45 +0200

    feat(cdba): multiple improvements for drawings table (DSCDA-2353)

--- apps/cdba/src/app/detail/detail-tab/drawings/drawings-table/drawings-table.component.spec.ts
+++ apps/cdba/src/app/detail/detail-tab/drawings/drawings-table/drawings-table.component.spec.ts
@@ -1,21 +1,12 @@
-import { SimpleChange } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 import { AgGridModule } from '@ag-grid-community/angular';
-import {
-  GridApi,
-  GridReadyEvent,
-  IStatusPanelParams,
-  RowSelectedEvent,
-} from '@ag-grid-enterprise/all-modules';
+import { RowSelectedEvent } from '@ag-grid-enterprise/all-modules';
 import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
 import { ReactiveComponentModule } from '@ngrx/component';
 
 import { SharedModule } from '@cdba/shared';
-import { CustomLoadingOverlayComponent } from '@cdba/shared/components/table/custom-overlay/custom-loading-overlay/custom-loading-overlay.component';
-import { CustomNoRowsOverlayComponent } from '@cdba/shared/components/table/custom-overlay/custom-no-rows-overlay/custom-no-rows-overlay.component';
-import { CustomOverlayModule } from '@cdba/shared/components/table/custom-overlay/custom-overlay.module';
 
 import { ActionsCellRendererComponent } from './actions-cell-renderer/actions-cell-renderer.component';
 import { DrawingsTableComponent } from './drawings-table.component';
@@ -31,12 +22,7 @@ describe('DrawingsTableComponent', () => {
       SharedModule,
       MatButtonModule,
       MatIconModule,
-      AgGridModule.withComponents([
-        CustomLoadingOverlayComponent,
-        CustomNoRowsOverlayComponent,
-        ActionsCellRendererComponent,
-      ]),
-      CustomOverlayModule,
+      AgGridModule.withComponents([ActionsCellRendererComponent]),
       ReactiveComponentModule,
     ],
   });
@@ -48,53 +34,6 @@ describe('DrawingsTableComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  describe('ngOnChanges', () => {
-    it('should showLoadingOverlay when grid loaded and isLoading active', () => {
-      component['gridApi'] = ({
-        showLoadingOverlay: jest.fn(),
-      } as unknown) as GridApi;
-
-      // tslint:disable-next-line: no-lifecycle-call
-      component.ngOnChanges({
-        isLoading: ({
-          currentValue: true,
-        } as unknown) as SimpleChange,
-      });
-
-      expect(component['gridApi'].showLoadingOverlay).toHaveBeenCalled();
-    });
-
-    it('should do nothing with the overlays when gridApi is not loaded', () => {
-      component['gridApi'] = undefined;
-
-      // tslint:disable-next-line: no-lifecycle-call
-      component.ngOnChanges({
-        isLoading: ({
-          currentValue: true,
-        } as unknown) as SimpleChange,
-      });
-
-      // should just succeed - otherwise this test should throw an error
-    });
-
-    it('should hide loading spinner and show NoRowsOverlay when loading is done', () => {
-      component['gridApi'] = ({
-        showLoadingOverlay: jest.fn(),
-        showNoRowsOverlay: jest.fn(),
-      } as unknown) as GridApi;
-
-      // tslint:disable-next-line: no-lifecycle-call
-      component.ngOnChanges({
-        isLoading: ({
-          currentValue: false,
-        } as unknown) as SimpleChange,
-      });
-
-      expect(component['gridApi'].showLoadingOverlay).not.toHaveBeenCalled();
-      expect(component['gridApi'].showNoRowsOverlay).toHaveBeenCalled();
-    });
   });
 
   describe('onRowSelected', () => {
@@ -114,105 +53,6 @@ describe('DrawingsTableComponent', () => {
       component.onRowSelected(event);
 
       expect(component.selectionChange.emit).toHaveBeenCalledWith(expected);
-    });
-  });
-
-  describe('onFirstDataRendered', () => {
-    const params: IStatusPanelParams = ({
-      columnApi: {
-        autoSizeAllColumns: jest.fn(),
-      },
-    } as unknown) as IStatusPanelParams;
-
-    beforeEach(() => {
-      component['gridApi'] = ({
-        getRowNode: jest.fn(() => ({ setSelected: jest.fn() })),
-      } as unknown) as GridApi;
-    });
-
-    it('should set node selected if nodeId is set', () => {
-      component.selectedNodeId = '7';
-
-      component.onFirstDataRendered(params);
-
-      expect(component['gridApi'].getRowNode).toHaveBeenCalled();
-    });
-
-    it('should do nothing, if nodeId is not present', () => {
-      component.selectedNodeId = undefined;
-
-      component.onFirstDataRendered(params);
-
-      expect(component['gridApi'].getRowNode).not.toHaveBeenCalled();
-    });
-
-    it('should autosize all columns', () => {
-      component.onFirstDataRendered(params);
-
-      expect(params.columnApi.autoSizeAllColumns).toHaveBeenCalledWith(false);
-    });
-  });
-
-  describe('onGridReady', () => {
-    const params = ({
-      api: {
-        showNoRowsOverlay: jest.fn(),
-      },
-    } as unknown) as GridReadyEvent;
-
-    beforeEach(() => {
-      jest.useFakeTimers();
-
-      component['refreshHeaders'] = jest.fn();
-    });
-
-    afterEach(() => {
-      // this is a workaround:
-      // only using useFakeTimers will affect all following specs, which leads to failures
-      jest.useRealTimers();
-    });
-
-    it('should set grid api', () => {
-      component.isLoading = true;
-
-      component.onGridReady(params);
-
-      expect(component['gridApi']).toEqual(params.api);
-    });
-
-    it('should hide loading spinner when data is not loading', () => {
-      component.isLoading = false;
-
-      component.onGridReady(params);
-
-      expect(component['gridApi'].showNoRowsOverlay).toHaveBeenCalled();
-    });
-
-    it('should call refreshHeaders', () => {
-      component.onGridReady(params);
-
-      expect(setTimeout).toHaveBeenCalledTimes(1);
-      expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 50);
-      jest.advanceTimersByTime(51);
-      expect(component['refreshHeaders']).toHaveBeenCalled();
-    });
-  });
-
-  describe('refreshHeaders', () => {
-    beforeEach(() => {
-      component['gridApi'] = ({
-        showNoRowsOverlay: jest.fn(),
-        refreshHeader: jest.fn(),
-        getColumnDef: jest.fn((colId) => ({
-          headerName: colId,
-        })),
-      } as unknown) as GridApi;
-    });
-
-    it('should call refreshHeader of gridApi', () => {
-      component['refreshHeaders']();
-
-      expect(component['gridApi'].refreshHeader).toHaveBeenCalled();
     });
   });
 });
--- apps/cdba/src/app/detail/detail-tab/drawings/drawings-table/drawings-table.component.ts
+++ apps/cdba/src/app/detail/detail-tab/drawings/drawings-table/drawings-table.component.ts
@@ -3,26 +3,15 @@ import {
   Component,
   EventEmitter,
   Input,
-  OnChanges,
   Output,
-  SimpleChanges,
 } from '@angular/core';
 
 import {
   ClientSideRowModelModule,
   ColDef,
-  GridApi,
-  GridReadyEvent,
-  IStatusPanelParams,
   RowSelectedEvent,
 } from '@ag-grid-enterprise/all-modules';
-import { translate } from '@ngneat/transloco';
 
-import { CustomLoadingOverlayComponent } from '@cdba/shared/components/table/custom-overlay/custom-loading-overlay/custom-loading-overlay.component';
-import {
-  CustomNoRowsOverlayComponent,
-  NoRowsParams,
-} from '@cdba/shared/components/table/custom-overlay/custom-no-rows-overlay/custom-no-rows-overlay.component';
 import { Drawing } from '@cdba/shared/models';
 
 import { ActionsCellRendererComponent } from './actions-cell-renderer/actions-cell-renderer.component';
@@ -34,13 +23,9 @@ import { COLUMN_DEFINITIONS, DEFAULT_COLUMN_DEFINITION } from './config';
   styleUrls: ['./drawings-table.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DrawingsTableComponent implements OnChanges {
-  private gridApi: GridApi;
-
+export class DrawingsTableComponent {
   @Input() rowData: Drawing[];
   @Input() selectedNodeId: string;
-  @Input() isLoading: boolean;
-  @Input() errorMessage: string;
 
   @Output() readonly selectionChange: EventEmitter<{
     nodeId: string;
@@ -54,69 +39,13 @@ export class DrawingsTableComponent implements OnChanges {
   public columnDefs: ColDef[] = COLUMN_DEFINITIONS;
 
   public frameworkComponents = {
-    customLoadingOverlay: CustomLoadingOverlayComponent,
-    customNoRowsOverlay: CustomNoRowsOverlayComponent,
     actionsCellRenderer: ActionsCellRendererComponent,
   };
 
-  public noRowsOverlayComponentParams: NoRowsParams = {
-    getMessage: () => this.errorMessage,
-  };
-
-  public loadingOverlayComponent = 'customLoadingOverlay';
-  public noRowsOverlayComponent = 'customNoRowsOverlay';
-
-  public ngOnChanges(changes: SimpleChanges): void {
-    if (!this.gridApi) {
-      return;
-    }
-
-    if (changes.isLoading && changes.isLoading.currentValue) {
-      this.gridApi.showLoadingOverlay();
-    } else {
-      this.gridApi.showNoRowsOverlay();
-    }
-  }
-
-  /**
-   * Limit selected rows to a maximum of two
-   */
   public onRowSelected({ node, api }: RowSelectedEvent): void {
     const nodeId = node.id;
     const drawing = api.getSelectedRows()[0];
 
     this.selectionChange.emit({ nodeId, drawing });
-  }
-
-  public onFirstDataRendered(params: IStatusPanelParams): void {
-    if (this.selectedNodeId) {
-      this.gridApi
-        .getRowNode(this.selectedNodeId)
-        .setSelected(true, true, true);
-    }
-
-    params.columnApi.autoSizeAllColumns(false);
-  }
-
-  public onGridReady(params: GridReadyEvent): void {
-    this.gridApi = params.api;
-
-    setTimeout(() => this.refreshHeaders(), 50);
-
-    if (!this.isLoading) {
-      this.gridApi.showNoRowsOverlay();
-    }
-  }
-
-  private refreshHeaders(): void {
-    this.columnDefs
-      .filter((colDef) => colDef.colId !== 'checkbox')
-      .map((colDef) => colDef.colId)
-      .forEach((colId) => {
-        const columnDef = this.gridApi.getColumnDef(colId);
-        columnDef.headerName = translate(columnDef.headerName);
-      });
-
-    this.gridApi.refreshHeader();
   }
 }
