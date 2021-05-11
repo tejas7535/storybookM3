8e8b6d5dd75b390452d8e8d4f7edbde40d364d69
herpisef <herpisef@schaeffler.com>
2021-05-11 10:45:53 +0200

    feat(ia): adapted env for D and P (IA-125)

--- apps/ia/src/environments/environment.prod.ts
+++ apps/ia/src/environments/environment.prod.ts
@@ -7,8 +7,8 @@ export const environment = {
 
   // AAD auth
   tenantId: '67416604-6509-4014-9859-45e709f53d3f',
-  clientId: '1345d78e-c59f-4cf3-9086-69953e8dc995',
-  appScope: 'api://54579610-09f1-48ab-83d7-ecfca5ab7436/iaapi',
+  clientId: '3ecfa7b3-44c9-4c34-819c-9a64d9ca936b',
+  appScope: 'api://08db73dd-5b75-443f-a496-7aa854712bfa/iaapi',
 
   // Application Insights
   applicationInsights: {
