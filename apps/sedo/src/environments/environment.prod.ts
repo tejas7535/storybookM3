3c1f8a72a6ea816531c0639e345a585394d691bb
Thomas Birke <birketho@schaeffler.com>
2021-06-02 08:13:27 +0200

    fix(sedo): adjust prod appId

--- apps/sedo/src/environments/environment.prod.ts
+++ apps/sedo/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   devToolsEnabled: false,
   tenantId: '67416604-6509-4014-9859-45e709f53d3f',
-  clientId: 'c2fbf9c3-e2f9-48a3-8483-57d946908949',
+  clientId: 'ca5945c1-e722-45af-9668-bd72c1ebdf00',
   appScope: 'api://c2fbf9c3-e2f9-48a3-8483-57d946908949/seliapi',
   apiBaseUrl: '/api/v1',
   applicationInsights: {
