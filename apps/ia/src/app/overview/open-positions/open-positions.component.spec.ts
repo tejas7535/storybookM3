b0db4f7c25f000b0b15135fe6b4961717649b5ef
Maciej Przybysz <przybmci@schaeffler.com>
2021-07-14 15:08:07 +0200

    fix(ia): open applications calculation (IA-191)

--- apps/ia/src/app/overview/open-positions/open-positions.component.spec.ts
+++ apps/ia/src/app/overview/open-positions/open-positions.component.spec.ts
@@ -3,6 +3,7 @@ import { createComponentFactory, Spectator } from '@ngneat/spectator';
 import { provideTranslocoTestingModule } from '@schaeffler/transloco';
 
 import { SharedModule } from '../../shared/shared.module';
+import { OpenApplication } from '../models';
 import { OpenPositionsComponent } from './open-positions.component';
 
 describe('OpenPositionsComponent', () => {
@@ -31,6 +32,35 @@ describe('OpenPositionsComponent', () => {
       const result = component.trackByFn(index);
 
       expect(result).toBe(index);
+    });
+  });
+
+  describe('countAllOpenApplications', () => {
+    it('should return undefined when no open applications set', () => {
+      const result = component.countOpenPositions();
+
+      expect(result).toBeUndefined();
+    });
+
+    it('should return count of open positions', () => {
+      component.openApplications = [
+        { count: 2 },
+        { count: 4 },
+        { count: 1 },
+        { count: 0 },
+      ] as OpenApplication[];
+
+      const result = component.countOpenPositions();
+
+      expect(result).toEqual(7);
+    });
+
+    it('should return 0 when open applications empty', () => {
+      component.openApplications = [];
+
+      const result = component.countOpenPositions();
+
+      expect(result).toEqual(0);
     });
   });
 });
