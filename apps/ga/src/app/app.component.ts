54f9b74f896733eb80799fa1f8e5b1da155fc927
Juskiw, Borys  SW/WRO-DCDZC <juskibry@schaeffler.com>
2021-10-14 15:18:15 +0200

    feat(ga): add icons (UFTABI-5038) (#3059)
    * feat(ga): add icons
    
    * feat(ga): add app preview
    
    * feat(ga): additional icons
    
    * feat(ga): update description
    
    * fix(ga): correct asset paths
    
    * feat(ga): add safari pinned tab rel
    
    Co-authored-by: Borys Juskiw <juskibry@schaeffler.com>

--- apps/ga/src/app/app.component.ts
+++ apps/ga/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Meta, MetaDefinition } from '@angular/platform-browser';
 import { translate } from '@ngneat/transloco';
 
 import { FooterLink } from '@schaeffler/footer';
@@ -11,6 +11,20 @@ import { LegalPath, LegalRoute } from '@schaeffler/legal-pages';
 })
 export class AppComponent {
   title = 'Grease App';
+  metaTags: MetaDefinition[] = [
+    { name: 'title', content: translate('meta.title') },
+    { name: 'description', content: translate('meta.description') },
+    // Open Graph / Facebook
+    { name: 'og:title', content: translate('meta.title') },
+    { name: 'og:description', content: translate('meta.description') },
+    // Twitter
+    { name: 'twitter:title', content: translate('meta.title') },
+    { name: 'twitter:description', content: translate('meta.description') },
+  ];
+
+  public constructor(private readonly meta: Meta) {
+    this.meta.addTags(this.metaTags);
+  }
 
   public footerLinks: FooterLink[] = [
     {
