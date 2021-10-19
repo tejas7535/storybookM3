de9afecbb996a250cf2fa4e14025d7409976c0ec
Geissler, Benjamin  SF/HZA-ZC2T <geissbnj@schaeffler.com>
2021-10-19 09:45:40 +0200

    feat(goldwind): add local environment + mock server (#3094)
    * feat(goldwind): add local environment including mock server
    
    * feat(goldwind): add local configuration for goldwind to workspace
    
    * chore(goldwind): remove unnessary line
    
    Co-authored-by: geissbnj <geissbnj@schaeffler.com>

--- apps/goldwind/src/environments/environment.local.ts
+++ apps/goldwind/src/environments/environment.local.ts
@@ -2,15 +2,12 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-import { Environment } from './environment.model';
-
-export const environment: Environment = {
+export const environment = {
   production: false,
-  localDev: true,
   devToolsEnabled: true,
-  publicSecurityBureauNumber: '沪公网安备31011402009389号',
-  icpnumber: '沪ICP备16014076号',
-  baseUrl: 'https://smartwindsolutions-dev.schaefflercn.com/api',
+  publicSecurityBureauNumber: 'LOCAL_TESTING',
+  icpnumber: '- ENVIRONMENT',
+  baseUrl: 'http://localhost:4010',
   tenantId: '5526d4ee-cae5-4c8e-ab8e-835b8be314f3',
   clientId: 'aba84ac1-d4d3-4a24-a2a9-79ce04c0f4e6',
   appScope: 'api://46f25cd6-752e-497a-8f06-410260db3042/ROL_USER',
