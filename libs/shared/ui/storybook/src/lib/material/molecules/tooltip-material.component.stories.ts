f79b8addc020feec4d1cde5ed6d5367997722904
Krause, Robert  SF/HZA-ZC2C <krausrbe@schaeffler.com>
2021-11-03 10:42:11 +0100

    feat(storybook): complete tooltip

--- libs/shared/ui/storybook/src/lib/material/molecules/tooltip-material.component.stories.ts
+++ libs/shared/ui/storybook/src/lib/material/molecules/tooltip-material.component.stories.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -9,40 +9,57 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { Meta, moduleMetadata, Story } from '@storybook/angular';
+import {
+  NavigationAtomic,
+  NavigationMain,
+} from 'libs/shared/ui/storybook/.storybook/storybook-navigation.constants';
 import { withDesign } from 'storybook-addon-designs';
 
+import { Badges } from '../../../../.storybook/storybook-badges.constants';
 import READMEMd from './tooltip/README.md';
 
 @Component({
   selector: 'toolip-component-example',
   template: `
-    <mat-form-field appearance="fill" class="mr-4 mb-8">
-      <mat-label>Tooltip Position</mat-label>
-      <mat-select [(value)]="position">
-        <mat-option value="below">Below</mat-option>
-        <mat-option value="above">Above</mat-option>
-        <mat-option value="left">Left</mat-option>
-        <mat-option value="right">Right</mat-option>
-      </mat-select>
-      <mat-hint>Configure the tooltip position</mat-hint>
-    </mat-form-field>
-
-    <button
-      matTooltip="Info about the action"
-      [matTooltipPosition]="'left'"
-      data-cy="showInformationToast"
-      mat-raised-button
-    >
-      Show Tooltip
-    </button>
+    <div class="flex flex-col gap-y-32">
+      <div>
+        <mat-form-field appearance="fill" class="mr-4 mb-8">
+          <mat-label>Tooltip Position</mat-label>
+          <mat-select [(value)]="horizontalPosition">
+            <mat-option value="below">Below</mat-option>
+            <mat-option value="above">Above</mat-option>
+            <mat-option value="left">Left</mat-option>
+            <mat-option value="right">Right</mat-option>
+          </mat-select>
+          <mat-hint>Configure the tooltip position</mat-hint>
+        </mat-form-field>
+        <mat-form-field appearance="fill" class="mr-4 mb-8">
+          <mat-label>Message</mat-label>
+          <input matInput [(ngModel)]="message" />
+          <mat-hint>
+            A long message will demonstrate the maximum width of the tooltip
+          </mat-hint>
+        </mat-form-field>
+      </div>
+      <div class="sm:ml-32 text-center sm:text-left">
+        <button
+          [matTooltip]="message"
+          [matTooltipPosition]="horizontalPosition"
+          mat-raised-button
+        >
+          Show Tooltip
+        </button>
+      </div>
+    </div>
   `,
 })
 class TooltipExampleComponent {
   horizontalPosition: TooltipPosition = 'above';
+  message: string = 'We pioneer motion';
 }
 
 export default {
-  title: 'Material/Molecules/Tooltip',
+  title: `${NavigationMain.Atomic}/${NavigationAtomic.Molecules}/Tooltip`,
   component: TooltipExampleComponent,
   decorators: [
     moduleMetadata({
@@ -50,7 +67,6 @@ export default {
         BrowserModule,
         BrowserAnimationsModule,
         FormsModule,
-        ReactiveFormsModule,
         MatButtonModule,
         MatFormFieldModule,
         MatInputModule,
@@ -62,6 +78,7 @@ export default {
   ],
   parameters: {
     notes: { markdown: READMEMd },
+    badges: [Badges.InProgress],
     design: {
       type: 'figma',
       url: 'https://www.figma.com/file/hhhgg57rQRgJ3YJwOHewZ9/DS-Test?node-id=749%3A21',
