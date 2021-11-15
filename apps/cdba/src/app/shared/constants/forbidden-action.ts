65810ed8435435ff145b7d30065b0b1bafe127d7
Tino Götz <t.goetz@cis-solutions.eu>
2021-11-15 09:37:54 +0100

    feat(cdba): change action button link in "no-access" page (DSCDA-2294) (#3347)
    Co-authored-by: tinogoetz <t.goetz@cis-solutions.eu>

--- apps/cdba/src/app/shared/constants/forbidden-action.ts
+++ apps/cdba/src/app/shared/constants/forbidden-action.ts
@@ -1,4 +1,2 @@
-import { environment } from '@cdba/environments/environment';
-
-export const NO_ACCESS_ACTION = `mailto:it-support-sg@schaeffler.com?subject=Assignment Group: CDBA_Support_G; Category: CDBA_Access&body=Hi,\n\nplease grant access to the following CDBA application:\n\n${environment.envName}\n\nThank you.`;
+export const NO_ACCESS_ACTION = `https://schaefflerprod.service-now.com/sup?id=sc_cat_item&sys_id=ed6a579fdb780410fa8c40a505961911&sysparm_category=69bbcbde1b22c014503297539b4bcba0`;
 export const FORBIDDEN_ACTION = `https://sconnect.schaeffler.com/groups/cost-database-analytics/blog/2020/10/14/how-does-the-rights-management-works`;
