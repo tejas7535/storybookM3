f031d775785e0a42b656fd644db5820e140cac3a
Soehnlein, Pascal <soehnpsc@schaeffler.com>
2021-12-02 16:09:48 +0100

    refactor(storybook): refactor responsive story (#3432)
    Co-authored-by: Soehnlein, Pascal  SF/HZA-ZC2A <soehnpsc@schaeffler.com>
    Co-authored-by: Krause, Robert  SF/HZA-ZC2C <krausrbe@schaeffler.com>

--- libs/shared/ui/storybook/src/lib/atomic/atoms/responsive.component.stories.ts
+++ libs/shared/ui/storybook/src/lib/atomic/atoms/responsive.component.stories.ts
@@ -2,12 +2,12 @@ import { Meta, Story } from '@storybook/angular';
 
 import { ForbiddenComponent } from '@schaeffler/empty-states';
 
-import READMEMd from '../../../../../empty-states/src/lib/forbidden/README.md';
 import { Badges } from '../../../../.storybook/storybook-badges.constants';
 import {
   NavigationAtomic,
   NavigationMain,
 } from '../../../../.storybook/storybook-navigation.constants';
+import READMEMd from './responsive/README.md';
 
 export default {
   title: `${NavigationMain.Atomic}/${NavigationAtomic.Atoms}/Responsive`,
@@ -17,12 +17,16 @@ export default {
   },
 } as Meta<ForbiddenComponent>;
 
-const Template: Story = (args) => ({
+const DefaultTemplate: Story = (args) => ({
   props: args,
-  template,
+  template: defaultTemplate,
+});
+const ResponsiveGridTemplate: Story = (args) => ({
+  props: args,
+  template: responsiveGridTemplate,
 });
 
-const template = `
+const defaultTemplate = `
   <div class="bg-background p-4">
     <h5>Documentation of responsive breakpoints</h5>
     <div class="my-4">
@@ -67,4 +71,33 @@ const template = `
   </div>
 `;
 
-export const Default = Template.bind({});
+const responsiveGridTemplate = `
+<div class="mb-4">
+  <h5>Material.io responsive grid layout</h5>
+  <a 
+    class="text-primary"  
+    href="https://material.io/design/layout/responsive-layout-grid.html#breakpoints" 
+    target="_blank" >https://material.io/design/layout/responsive-layout-grid.html#breakpoints
+  </a>
+  <p>Adjust the storybook viewports for testing</p>
+</div>
+<div class="grid grid-cols-4 gap-4 sm:grid-cols-8  md:grid-cols-12 md:gap-6 h-full text-center">
+  <div class="h-full bg-[#018786] bg-opacity-[0.12]">1</div>
+  <div class="h-full bg-[#018786] bg-opacity-[0.12]">2</div>
+  <div class="h-full bg-[#018786] bg-opacity-[0.12]">3</div>
+  <div class="h-full bg-[#018786] bg-opacity-[0.12]">4</div>
+
+  <div class="hidden sm:grid h-full bg-[#018786] bg-opacity-[0.12]">5</div>
+  <div class="hidden sm:grid h-full bg-[#018786] bg-opacity-[0.12]">6</div>
+  <div class="hidden sm:grid h-full bg-[#018786] bg-opacity-[0.12]">7</div>
+  <div class="hidden sm:grid h-full bg-[#018786] bg-opacity-[0.12]">8</div>
+
+  <div class="hidden md:grid h-full bg-[#018786] bg-opacity-[0.12]">9</div>
+  <div class="hidden md:grid h-full bg-[#018786] bg-opacity-[0.12]">10</div>
+  <div class="hidden md:grid h-full bg-[#018786] bg-opacity-[0.12]">11</div>
+  <div class="hidden md:grid  h-full bg-[#018786] bg-opacity-[0.12]">12</div>
+</div>
+`;
+
+export const Default = DefaultTemplate.bind({});
+export const ReponsiveGrid = ResponsiveGridTemplate.bind({});
---
+++ libs/shared/ui/storybook/src/lib/atomic/atoms/responsive/README.md
@@ -0,0 +1,5 @@
+# Responsive Breakpoint Documentation
+
+The usage of the Breakpoints is achieved with tailwinds [Responsive Design](https://tailwindcss.com/docs/responsive-design).
+
+The breakpoint minimum widths and the responsive grid layout is based on the Material Design [Breakpoint system](https://material.io/design/layout/responsive-layout-grid.html#breakpoints).
