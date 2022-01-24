49629389ef3b464b30ff788b0fad4186a4728d16
Fabian Kaupp <kauppfbi@schaeffler.com>
2022-01-24 16:13:35 +0100

    chore(sedo): update ag grid license key

--- apps/sedo/src/test-setup.ts
+++ apps/sedo/src/test-setup.ts
@@ -5,6 +5,6 @@ import { LicenseManager } from '@ag-grid-enterprise/all-modules';
 // eslint-disable-next-line import/order
 global.beforeEach(() => {
   LicenseManager.setLicenseKey(
-    `CompanyName=Comparex AG (Leipzig)_on_behalf_of_Schaeffler Technologies AG & Co,LicensedApplication=angular,LicenseType=SingleApplication,LicensedConcurrentDeveloperCount=9,LicensedProductionInstancesCount=0,AssetReference=AG-011662,ExpiryDate=5_November_2021_[v2]_MTYzNjA3MDQwMDAwMA==685d64a76556dd1ffc78a59025dc6b39`
+    `CompanyName=SoftwareOne AG (Leipzig)_on_behalf_of_Schaeffler Technologies AG & Co,LicensedGroup=Data Science Solutions,LicenseType=MultipleApplications,LicensedConcurrentDeveloperCount=5,LicensedProductionInstancesCount=1,AssetReference=AG-022173,ExpiryDate=14_January_2023_[v2]_MTY3MzY1NDQwMDAwMA==c393351117eb419cc1aec4eed573b05e`
   );
 });
