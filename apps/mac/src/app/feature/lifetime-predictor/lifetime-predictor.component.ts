50821541a316fe3192873ba3ae3289c609935e63
Krause, Robert  SF/HZA-ZC2C <krausrbe@schaeffler.com>
2022-01-27 15:10:49 +0100

    feat(mac): remove @schaeffler/responsive usage (#3521)

--- apps/mac/src/app/feature/lifetime-predictor/lifetime-predictor.component.ts
+++ apps/mac/src/app/feature/lifetime-predictor/lifetime-predictor.component.ts
@@ -1,11 +1,11 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 import { Store } from '@ngrx/store';
 
 import { ApplicationInsightsService } from '@schaeffler/application-insights';
-import { BreakpointService } from '@schaeffler/responsive';
 
 import * as fromStore from './store';
 
@@ -16,18 +16,18 @@ import * as fromStore from './store';
 })
 export class LifetimePredictorComponent implements OnInit {
   public isLessThanMediumViewPort$: Observable<boolean>;
-  public isMobileViewPort$: Observable<boolean>;
 
   public constructor(
     private readonly store: Store,
-    private readonly breakpointService: BreakpointService,
+    private readonly breakpointObserver: BreakpointObserver,
     private readonly applicationInsightService: ApplicationInsightsService
   ) {}
 
   public ngOnInit(): void {
     this.applicationInsightService.logEvent('[MAC - LTP] opened');
-    this.isLessThanMediumViewPort$ = this.breakpointService.isLessThanMedium();
-    this.isMobileViewPort$ = this.breakpointService.isMobileViewPort();
+    this.isLessThanMediumViewPort$ = this.breakpointObserver
+      .observe([Breakpoints.XSmall, Breakpoints.Small])
+      .pipe(map((state) => state.matches));
   }
 
   public handleReset(): void {
