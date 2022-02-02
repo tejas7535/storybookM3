df7a5364ae0de1489b994839f52699c23747ad64
Krause, Robert  SF/HZA-ZC2C <krausrbe@schaeffler.com>
2022-02-02 13:21:15 +0100

    fix(ga): environment impact typo (#3664)
    Co-authored-by: Krause, Robert  SF/HZA-ZC2C <krausrbe@schaeffler.com>
    Co-authored-by: Kaupp, Fabian  SF/HZA-ZC2A <kauppfbi@schaeffler.com>

--- apps/ga/src/app/shared/models/parameters/environment-impact.model.ts
+++ apps/ga/src/app/shared/models/parameters/environment-impact.model.ts
@@ -1,5 +1,5 @@
 export enum EnvironmentImpact {
   low = 'LB_LOW_AMBIENT_INFLUENCE',
   moderate = 'LB_AVERAGE_AMBIENT_INFLUENCE',
-  high = 'LB_HIGHT_AMBIENT_INFLUENCE',
+  high = 'LB_HIGH_AMBIENT_INFLUENCE',
 }
