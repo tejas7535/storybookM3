fc9bbb41d7fe54ce479241fbb4fa67aa87b0cc01
Krause, Robert  SF/HZA-ZC2C <krausrbe@schaeffler.com>
2022-02-22 09:04:32 +0100

    feat(mm): use updated data privacy and purpose

--- apps/mm/src/app/app.module.ts
+++ apps/mm/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { TranslocoService } from '@ngneat/transloco';
 import { ReactiveComponentModule } from '@ngrx/component';
 
 import { AppShellModule } from '@schaeffler/app-shell';
-import { PERSON_RESPONSIBLE } from '@schaeffler/legal-pages';
+import { PERSON_RESPONSIBLE, PURPOSE } from '@schaeffler/legal-pages';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +13,10 @@ import { CoreModule } from './core/core.module';
 import { StoreModule } from './core/store/store.module';
 import { responsiblePerson } from './shared/constants/legal-constants';
 import { MaterialModule } from './shared/material.module';
+
+export function DynamicPurpose(translocoService: TranslocoService) {
+  return translocoService.selectTranslateObject('legal.purpose');
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -28,6 +33,11 @@ import { MaterialModule } from './shared/material.module';
     {
       provide: PERSON_RESPONSIBLE,
       useValue: responsiblePerson,
+    },
+    {
+      provide: PURPOSE,
+      useFactory: DynamicPurpose,
+      deps: [TranslocoService],
     },
   ],
   bootstrap: [AppComponent],
