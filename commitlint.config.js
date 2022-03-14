dc0bc7f2c4daed045c646b22bae06021ce05cb97
Kaupp, Fabian  SF/HZA-ZC2A <kauppfbi@schaeffler.com>
2022-03-14 08:46:33 +0100

    feat(workspace): update commitlint configuration (#3865)
    commitlint now enforces to use specific scopes, basically our project names. In addition, the
    commitizen adapter was changed, so that the prompt-cli (git-cz) properly considers the commitlint
    configuration
    
    Co-authored-by: Krause, Robert  SF/HZA-ZC2C <krausrbe@schaeffler.com>

---
+++ commitlint.config.js
@@ -0,0 +1,42 @@
+const { Workspaces } = require('@nrwl/tao/src/shared/workspace');
+
+const Configuration = {
+  extends: ['@commitlint/config-conventional'],
+  utils: { getProjects },
+  rules: {
+    'scope-enum': (ctx) =>
+      getProjects(ctx).then((packages) => [2, 'always', packages]),
+  },
+};
+
+const nonNxProjectScopes = ['deps', 'jenkinsfile', 'workspace'];
+
+function getProjects(context) {
+  return Promise.resolve()
+    .then(() => {
+      const ctx = context || {};
+      const cwd = ctx.cwd || process.cwd();
+      const ws = new Workspaces(cwd);
+      const workspace = ws.readWorkspaceConfiguration();
+      return Object.entries(workspace.projects || {}).map(
+        ([name, project]) => ({
+          name,
+          ...project,
+        })
+      );
+    })
+    .then((projects) => {
+      return projects
+        .filter((project) => project.targets)
+        .map((project) => project.name)
+        .map((name) =>
+          name
+            .replaceAll('shared-', '')
+            .replaceAll('utils-', '')
+            .replaceAll('ui-', '')
+        )
+        .concat(nonNxProjectScopes);
+    });
+}
+
+module.exports = Configuration;
