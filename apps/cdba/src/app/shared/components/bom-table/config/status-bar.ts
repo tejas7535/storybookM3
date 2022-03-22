7c14e27f06cfe494517ab7d1fa532ee004e65492
tinogoetz <t.goetz@cis-solutions.eu>
2022-03-22 17:23:18 +0100

    feat(cdba): enable aggregation component in bom table (DSCDA-2892)

--- apps/cdba/src/app/shared/components/bom-table/config/status-bar.ts
+++ apps/cdba/src/app/shared/components/bom-table/config/status-bar.ts
@@ -3,5 +3,13 @@ import { StatusPanelDef } from '@ag-grid-enterprise/all-modules';
 export const BOM_TABLE_STATUS_BAR_CONFIG: {
   statusPanels: StatusPanelDef[];
 } = {
-  statusPanels: [{ statusPanel: 'bomTableStatusBar', align: 'left' }],
+  statusPanels: [
+    { statusPanel: 'bomTableStatusBar', align: 'left' },
+    {
+      statusPanel: 'agAggregationComponent',
+      statusPanelParams: {
+        aggFuncs: ['count', 'sum', 'min', 'max', 'avg'],
+      },
+    },
+  ],
 };
