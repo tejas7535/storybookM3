f502934c50af98a42e29cc41c1fbba36a9a3904f
Kaupp, Fabian  SF/HZA-ZC2A <kauppfbi@schaeffler.com>
2022-03-29 08:34:49 +0200

    fix(cdba): correct RowNode import

--- apps/cdba/src/app/shared/components/table/radio-button-cell-render/radio-button-cell-render.component.ts
+++ apps/cdba/src/app/shared/components/table/radio-button-cell-render/radio-button-cell-render.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { RowNode } from '@ag-grid-community/core/dist/cjs/entities/rowNode';
+import { RowNode } from '@ag-grid-community/all-modules';
 import { ICellRendererParams } from '@ag-grid-enterprise/all-modules';
 
 @Component({
