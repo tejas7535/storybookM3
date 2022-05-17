43367c76e16067db3c1c91c4a272c8f4b971f579
Soehnlein, Pascal  SF/HZA-ZC2A <soehnpsc@schaeffler.com>
2022-05-17 12:12:40 +0200

    fix(gq): fix color of margin warning (GQUOTE-1342) (#4137)
    Co-authored-by: Pascal Söhnlein <soehnpsc@schaeffler.com>

--- apps/gq/proxy.conf.js
+++ apps/gq/proxy.conf.js
@@ -1,4 +1,5 @@
-const PROXY_CONFIG = [
+var HttpsProxyAgent = require('https-proxy-agent');
+const proxyConfig = [
   {
     context: ['/api'],
     // target: 'http://localhost:8080',
@@ -8,4 +9,16 @@ const PROXY_CONFIG = [
   },
 ];
 
-module.exports = PROXY_CONFIG;
+function setupForCorporateProxy(proxyConfig) {
+  var proxyServer = process.env.http_proxy || process.env.HTTP_PROXY;
+  if (proxyServer) {
+    var agent = new HttpsProxyAgent(proxyServer);
+    console.log('Using corporate proxy server: ' + proxyServer);
+    proxyConfig.forEach(function (entry) {
+      entry.agent = agent;
+    });
+  }
+  return proxyConfig;
+}
+
+module.exports = setupForCorporateProxy(proxyConfig);
