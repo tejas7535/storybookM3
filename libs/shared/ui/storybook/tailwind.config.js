09f195ad0fe9f120fc78e5ca1a42de99ee281fdd
tinogoetz <t.goetz@cis-solutions.eu>
2022-06-10 18:17:12 +0200

    fix(storybook): add missing styles and tw config

---
+++ libs/shared/ui/storybook/tailwind.config.js
@@ -0,0 +1,10 @@
+const { createGlobPatternsForDependencies } = require('@nrwl/angular/tailwind');
+const { join } = require('path');
+
+module.exports = {
+  presets: [require('../../../../tailwind.config')],
+  content: [
+    join(__dirname, 'src/**/*.ts'),
+    ...createGlobPatternsForDependencies(__dirname, '../**/*.ts}'),
+  ],
+};
