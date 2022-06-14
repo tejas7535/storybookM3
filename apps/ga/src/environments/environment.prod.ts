c504df868a8bc0bdcf0bedf31c4fc883d0787aca
tinogoetz <t.goetz@cis-solutions.eu>
2022-06-14 15:03:18 +0200

    chore(ga): change one trust id

--- apps/ga/src/environments/environment.prod.ts
+++ apps/ga/src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment: Environment = {
   baseUrl: 'https://bearinx.schaeffler.com/api/v1.2/greaseservice',
   tenantId: '035a32aa-f517-4698-a818-e756af53c99e',
   groupId: 'c4c115f9-a73a-4761-a7d1-daa07e6deda1',
-  oneTrustId: 'a3bfa9ba-5b96-461d-ab88-62efed434c87',
+  oneTrustId: 'ffed3897-cb7b-4db2-bd45-3a93e8c57636',
   applicationInsights: {
     applicationInsightsConfig: {
       instrumentationKey: 'cfecb843-072b-4edf-9c7d-2b52d7309d6d',
