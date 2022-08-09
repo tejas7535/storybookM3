6f20f2767311e97a478667b03f4018912fa22209
Soehnlein, Pascal  SF/HZA-ZC2A <soehnpsc@schaeffler.com>
2022-08-09 14:58:16 +0200

    fix(gq): fix disabled localization (#4477)

--- apps/gq/src/app/shared/components/user-settings/user-settings.component.ts
+++ apps/gq/src/app/shared/components/user-settings/user-settings.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 import { Locale } from '@schaeffler/transloco/components';
 
-import { environment } from '../../../../environments/environment';
 import { AVAILABLE_LOCALES, serivceNowAdress } from '../../constants';
 
 @Component({
@@ -10,7 +9,6 @@ import { AVAILABLE_LOCALES, serivceNowAdress } from '../../constants';
   templateUrl: './user-settings.component.html',
 })
 export class UserSettingsComponent {
-  production = environment.production;
   serivceNowAdress = serivceNowAdress;
   availableLocales: Locale[] = AVAILABLE_LOCALES;
   defaultLocale: Locale = AVAILABLE_LOCALES[0];
