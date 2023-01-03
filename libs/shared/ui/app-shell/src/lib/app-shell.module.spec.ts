2156792162e6e056561b16c9427f787bb9da8878
Kaupp, Fabian  SF/HZA-ZC2S <kauppfbi@schaeffler.com>
2023-01-03 15:19:45 +0100

    fix(app-shell): add translation files to npm package (#4907)

--- libs/shared/ui/app-shell/src/lib/app-shell.module.spec.ts
+++ libs/shared/ui/app-shell/src/lib/app-shell.module.spec.ts
@@ -1,13 +1,30 @@
+import { TranslocoService } from '@ngneat/transloco';
+
 import { AppShellModule } from './app-shell.module';
+import deJson from './i18n/de.json';
+import enJson from './i18n/en.json';
+import esJson from './i18n/es.json';
+import frJson from './i18n/fr.json';
+import ruJson from './i18n/ru.json';
+import zhJson from './i18n/zh.json';
 
 describe('AppShellModule', () => {
-  let module: AppShellModule;
+  describe('setLanguageFiles', () => {
+    test('should set the german language file for "de"', () => {
+      const serviceMock = {
+        setTranslation: jest.fn(),
+      } as unknown as TranslocoService;
 
-  beforeEach(() => {
-    module = new AppShellModule();
-  });
+      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+      new AppShellModule(serviceMock);
 
-  it('should be defined', () => {
-    expect(module).toBeDefined();
+      expect(serviceMock.setTranslation).toHaveBeenCalledTimes(6);
+      expect(serviceMock.setTranslation).toHaveBeenCalledWith(deJson, 'de');
+      expect(serviceMock.setTranslation).toHaveBeenCalledWith(enJson, 'en');
+      expect(serviceMock.setTranslation).toHaveBeenCalledWith(esJson, 'es');
+      expect(serviceMock.setTranslation).toHaveBeenCalledWith(frJson, 'fr');
+      expect(serviceMock.setTranslation).toHaveBeenCalledWith(ruJson, 'ru');
+      expect(serviceMock.setTranslation).toHaveBeenCalledWith(zhJson, 'zh');
+    });
   });
 });
--- libs/shared/ui/app-shell/src/lib/app-shell.module.ts
+++ libs/shared/ui/app-shell/src/lib/app-shell.module.ts
@@ -7,10 +7,16 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 
-import { TranslocoModule } from '@ngneat/transloco';
+import { TranslocoModule, TranslocoService } from '@ngneat/transloco';
 
 import { AppShellComponent } from './components/app-shell/app-shell.component';
 import { UserPanelComponent } from './components/user-panel/user-panel.component';
+import deJson from './i18n/de.json';
+import enJson from './i18n/en.json';
+import esJson from './i18n/es.json';
+import frJson from './i18n/fr.json';
+import ruJson from './i18n/ru.json';
+import zhJson from './i18n/zh.json';
 
 @NgModule({
   imports: [
@@ -26,4 +32,13 @@ import { UserPanelComponent } from './components/user-panel/user-panel.component
   declarations: [AppShellComponent, UserPanelComponent],
   exports: [AppShellComponent],
 })
-export class AppShellModule {}
+export class AppShellModule {
+  public constructor(private readonly translocoService: TranslocoService) {
+    this.translocoService.setTranslation(deJson, 'de');
+    this.translocoService.setTranslation(enJson, 'en');
+    this.translocoService.setTranslation(esJson, 'es');
+    this.translocoService.setTranslation(frJson, 'fr');
+    this.translocoService.setTranslation(ruJson, 'ru');
+    this.translocoService.setTranslation(zhJson, 'zh');
+  }
+}
