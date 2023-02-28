f9d246340f7f727f4ca81875bb5c5d4a6da0eeef
Schlueter, Nils  (ext.)  SF/HZA-ZCQA <schlunls@schaeffler.com>
2023-02-28 19:32:53 +0100

    feat(gq): removed feature toggle for global search (GQUOTE-2277) (#5055)

--- apps/gq/src/feature-config/default-config.ts
+++ apps/gq/src/feature-config/default-config.ts
@@ -1,9 +1,7 @@
 export const DEFAULT_CONFIG: GqFeatureToggleConfig = {
-  globalSearch: false,
   approvalWorkflow: false,
 };
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface GqFeatureToggleConfig {
-  globalSearch: boolean;
   approvalWorkflow: boolean;
 }
