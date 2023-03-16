49326a0be9380cbbd2c366e2fb75b0a1ba87bae9
Krause, Robert  SF/HZA-ZC2C <krausrbe@schaeffler.com>
2023-03-16 11:12:00 +0100

    feat(ea): enable webcomponent functionality (#5098)

--- apps/ea/src/app/app.module.ts
+++ apps/ea/src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { PushModule } from '@ngrx/component';
 
 import { AppComponent } from './app.component';
-import { CalculationParametersComponent } from './features/calculation-parameters/calculation-parameters';
 import { CoreModule } from './core/core.module';
+import { CalculationParametersComponent } from './features/calculation-parameters/calculation-parameters';
 
 @NgModule({
   declarations: [AppComponent],
@@ -20,4 +21,12 @@ import { CoreModule } from './core/core.module';
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly injector: Injector) {}
+
+  // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
+  ngDoBootstrap() {
+    const el = createCustomElement(AppComponent, { injector: this.injector });
+    customElements.define('engineering-app', el);
+  }
+}
