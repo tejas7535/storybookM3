e7aa9d1885ee12c5515ff625ec14cfe7171f2f94
Vogel, Moritz  SF/HZA-ZC2C <vogelmor@schaeffler.com>
2023-09-12 16:03:02 +0200

    fix(ea): fix empty api urls to prevent interception (#5606)

--- apps/ea/src/environments/environment.prod.ts
+++ apps/ea/src/environments/environment.prod.ts
@@ -4,10 +4,12 @@ export const environment: Environment = {
   production: true,
   localDev: false,
   devToolsEnabled: false,
-  catalogApiBaseUrl: '', // TODO
+  catalogApiBaseUrl:
+    'https://caeonlinecalculation-q.schaeffler.com/CatalogWebApi', // TODO use prod url
   frictionApiBaseUrl: 'https://bearinx-d.schaeffler.com/co_api', // TODO use prod url in final deployment
   co2UpstreamApiBaseUrl: 'https://co2-api.dp.schaeffler/api',
-  calculationModuleInfoApiBaseUrl: '', // TODO
+  calculationModuleInfoApiBaseUrl:
+    'https://bearinx-d.schaeffler.com/moduleinfo/api/v1/CalculationModuleInfo', // TODO
   tenantId: '035a32aa-f517-4698-a818-e756af53c99e',
   groupId: 'c4c115f9-a73a-4761-a7d1-daa07e6deda1',
   assetsPath: 'https://engineeringapp.com/assets',
