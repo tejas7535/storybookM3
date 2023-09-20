fc084efb471e7f5f0941ee87eed0b092daf89c7a
Yankov, Radoslav  (ext.)  SF/HZA-ZCQC <yankordo@schaeffler.com>
2023-09-20 12:53:53 +0200

    fix(mac): enable html as tooltip value (#5630)

--- apps/mac/src/app/feature/materials-supplier-database/main-table/header-tooltip/header-tooltip.component.ts
+++ apps/mac/src/app/feature/materials-supplier-database/main-table/header-tooltip/header-tooltip.component.ts
@@ -10,9 +10,8 @@ import { TooltipParams } from '../table-config';
   template: `
     <div
       class="whitespace-pre-line rounded bg-secondary-900 py-2 px-4 text-caption text-white"
-    >
-      {{ tooltipValue }}
-    </div>
+      [innerHTML]="tooltipValue"
+    ></div>
   `,
 })
 export class HeaderTooltipComponent implements ITooltipAngularComp {
