1c82ff4aaf1f3cba33dcf202b7c0de48a06cce99
Vogel, Moritz  SF/HZA-ZC2C <vogelmor@schaeffler.com>
2023-10-11 08:32:21 +0200

    fix(ea): adjust api urls for production environment (#5707)

--- apps/ea/src/environments/environment.prod.ts
+++ apps/ea/src/environments/environment.prod.ts
@@ -4,11 +4,11 @@ export const environment: Environment = {
   production: true,
   localDev: false,
   devToolsEnabled: false,
-  catalogApiBaseUrl: 'https://catalogcalculations-cae.schaeffler.com/api',
+  catalogApiBaseUrl: 'https://bearinx-d.schaeffler.com/catalogue/api',
   frictionApiBaseUrl: 'https://bearinx-d.schaeffler.com/co_api', // TODO use prod url in final deployment
   co2UpstreamApiBaseUrl: 'https://co2-api.dp.schaeffler/api',
   calculationModuleInfoApiBaseUrl:
-    'https://bearinx-d.schaeffler.com/moduleinfo/api/v1/CalculationModuleInfo', // TODO
+    'https://bearinx-d.schaeffler.com/moduleinfo/api', // TODO
   staticStorageUrl: 'https://frontend1apps.z1.web.core.windows.net',
   tenantId: '035a32aa-f517-4698-a818-e756af53c99e',
   groupId: 'c4c115f9-a73a-4761-a7d1-daa07e6deda1',
