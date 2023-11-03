a4c5fba3f94ee355ea3b92c2f57fa4d8a0609ac2
Soehnlein, Pascal  SF/HZA-ZC2A <soehnpsc@schaeffler.com>
2023-11-03 11:15:33 +0100

    fix(gq): add tooltip component for reference pricing table (#5765)

--- apps/gq/src/app/f-pricing/pricing-assistant-modal/pricing-tabs-wrapper/components/reference-pricing-table/config/components.ts
+++ apps/gq/src/app/f-pricing/pricing-assistant-modal/pricing-tabs-wrapper/components/reference-pricing-table/config/components.ts
@@ -1,7 +1,9 @@
 import { ReferenceMaterialGroupCellComponent } from '@gq/shared/ag-grid/cell-renderer/reference-material-group-cell/reference-material-group-cell.component';
 import { ShowMoreRowsComponent } from '@gq/shared/ag-grid/cell-renderer/show-more-rows/show-more-rows.component';
+import { ExtendedColumnHeaderComponent } from '@gq/shared/ag-grid/column-headers/extended-column-header/extended-column-header.component';
 
 export const COMPONENTS = {
   ReferenceMaterialGroupCellComponent,
   ShowMoreRowsComponent,
+  agColumnHeader: ExtendedColumnHeaderComponent,
 };
