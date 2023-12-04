9a7a125f72b64285afeaf4139c2dfda449a66258
Grzegorzewski, Damian  SW/WRO-DCDZC <grzegdmi@schaeffler.com>
2023-12-04 14:20:05 +0100

    feat(app-shell): add additional translations to app shell (#5828)

--- libs/shared/ui/app-shell/src/lib/app-shell.module.spec.ts
+++ libs/shared/ui/app-shell/src/lib/app-shell.module.spec.ts
@@ -1,12 +1,19 @@
 import { TranslocoService } from '@ngneat/transloco';
 
 import { AppShellModule } from './app-shell.module';
-import deJson from './i18n/de.json';
-import enJson from './i18n/en.json';
-import esJson from './i18n/es.json';
-import frJson from './i18n/fr.json';
-import ruJson from './i18n/ru.json';
-import zhJson from './i18n/zh.json';
+import deutsch from './i18n/de.json';
+import english from './i18n/en.json';
+import spanish from './i18n/es.json';
+import french from './i18n/fr.json';
+import indoenesianBahasa from './i18n/id.json';
+import italian from './i18n/it.json';
+import japanese from './i18n/ja.json';
+import korean from './i18n/ko.json';
+import russian from './i18n/ru.json';
+import thai from './i18n/th.json';
+import vietnamese from './i18n/vi.json';
+import chineseSimplified from './i18n/zh.json';
+import chineseTraditional from './i18n/zh_TW.json';
 
 describe('AppShellModule', () => {
   describe('setLanguageFiles', () => {
@@ -18,13 +25,34 @@ describe('AppShellModule', () => {
       // eslint-disable-next-line @typescript-eslint/no-unused-expressions
       new AppShellModule(serviceMock);
 
-      expect(serviceMock.setTranslation).toHaveBeenCalledTimes(6);
-      expect(serviceMock.setTranslation).toHaveBeenCalledWith(deJson, 'de');
-      expect(serviceMock.setTranslation).toHaveBeenCalledWith(enJson, 'en');
-      expect(serviceMock.setTranslation).toHaveBeenCalledWith(esJson, 'es');
-      expect(serviceMock.setTranslation).toHaveBeenCalledWith(frJson, 'fr');
-      expect(serviceMock.setTranslation).toHaveBeenCalledWith(ruJson, 'ru');
-      expect(serviceMock.setTranslation).toHaveBeenCalledWith(zhJson, 'zh');
+      expect(serviceMock.setTranslation).toHaveBeenCalledTimes(13);
+      expect(serviceMock.setTranslation).toHaveBeenCalledWith(deutsch, 'de');
+      expect(serviceMock.setTranslation).toHaveBeenCalledWith(english, 'en');
+      expect(serviceMock.setTranslation).toHaveBeenCalledWith(spanish, 'es');
+      expect(serviceMock.setTranslation).toHaveBeenCalledWith(french, 'fr');
+      expect(serviceMock.setTranslation).toHaveBeenCalledWith(
+        indoenesianBahasa,
+        'id'
+      );
+      expect(serviceMock.setTranslation).toHaveBeenCalledWith(italian, 'it');
+
+      expect(serviceMock.setTranslation).toHaveBeenCalledWith(japanese, 'ja');
+      expect(serviceMock.setTranslation).toHaveBeenCalledWith(russian, 'ru');
+      expect(serviceMock.setTranslation).toHaveBeenCalledWith(
+        chineseSimplified,
+        'zh'
+      );
+      expect(serviceMock.setTranslation).toHaveBeenCalledWith(
+        chineseTraditional,
+        'zh_TW'
+      );
+      expect(serviceMock.setTranslation).toHaveBeenCalledWith(
+        indoenesianBahasa,
+        'id'
+      );
+      expect(serviceMock.setTranslation).toHaveBeenCalledWith(korean, 'ko');
+      expect(serviceMock.setTranslation).toHaveBeenCalledWith(thai, 'th');
+      expect(serviceMock.setTranslation).toHaveBeenCalledWith(vietnamese, 'vi');
     });
   });
 });
