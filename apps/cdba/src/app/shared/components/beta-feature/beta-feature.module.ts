d6ba004cfab37f97021dfd95e7978c2ade0e29e8
Poltorak, Krzysztof  SW/WRO-DCDZC <poltokzy@schaeffler.com>
2023-12-18 14:20:13 +0100

    fix(cdba): fixes for found styling issues (#5867)
    * fix(cdba): added padding for details sections in compare screen
    
    * fix(cdba): added calculating max width of a tab
    
    * fix(cdba): layout fixes in beta popup window
    
    * fix(cdba): added bottom padding

--- apps/cdba/src/app/shared/components/beta-feature/beta-feature.module.ts
+++ apps/cdba/src/app/shared/components/beta-feature/beta-feature.module.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { SharedTranslocoModule } from '@schaeffler/transloco';
--- apps/cdba/src/app/shared/components/tabs-header/tabs-header.component.ts
+++ apps/cdba/src/app/shared/components/tabs-header/tabs-header.component.ts
@@ -5,6 +5,7 @@ import { Tab } from './tab.model';
 @Component({
   selector: 'cdba-tabs-header',
   templateUrl: './tabs-header.component.html',
+  styleUrls: ['./tabs-header.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TabsHeaderComponent {
