30854ebbc7092e6b6062a5ebb74f8a4b18b7b41c
Berndt, Christian  SF/HZA-ZC2C <berndcri@schaeffler.com>
2023-12-18 15:30:06 +0100

    feat(breadcrumbs): migrate to mdc components (#5832)

--- libs/shared/ui/breadcrumbs/src/lib/breadcrumbs.module.ts
+++ libs/shared/ui/breadcrumbs/src/lib/breadcrumbs.module.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { MatLegacyMenuModule as MatMenuModule } from '@angular/material/legacy-menu';
-import { MatLegacyTooltipModule as MatTooltipModule } from '@angular/material/legacy-tooltip';
 import { RouterModule } from '@angular/router';
 
 import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 import { BreadcrumbsItemComponent } from './components/breadcrumbs-item/breadcrumbs-item.component';
 import { BreadcrumbsMenuItemComponent } from './components/breadcrumbs-menu-item/breadcrumbs-menu-item.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
