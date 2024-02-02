93ab40ae50049898157e517f4ec598e3619522b8
Vogel, Moritz  SF/HZA-ZC2C <vogelmor@schaeffler.com>
2024-02-02 12:15:00 +0100

    fix(ga): fix selection not working on mobile - UFTABI-7260 (#5966)

--- apps/ga/src/app/home/home.component.ts
+++ apps/ga/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { ApplicationInsightsService } from '@schaeffler/application-insights';
 
@@ -10,6 +10,7 @@ import { HomeCardsService } from './services/home-cards.service';
 @Component({
   selector: 'ga-home',
   templateUrl: './home.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
   public appDelivery$ = this.settingsFacade.appDelivery$;
