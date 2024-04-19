41846745e2ca7e6f30772924b017f22d129c3011
Poltorak, Krzysztof  SW/WRO-DCDZC <poltokzy@schaeffler.com>
2024-04-19 14:10:51 +0200

    fix(cdba): corrected url (#6161)

--- apps/cdba/src/app/shared/constants/forbidden-action.ts
+++ apps/cdba/src/app/shared/constants/forbidden-action.ts
@@ -1,2 +1,2 @@
-export const NO_ACCESS_ACTION = `https://schaefflerprod.service-now.com/sup?id=sc_cat_item&sys_id=ed6a579fdb780410fa8c40a505961911&sysparm_category=69bbcbde1b22c014503297539b4bcba0`;
+export const NO_ACCESS_ACTION = `https://schaefflerprod.service-now.com/sup?id=sc_cat_item&sys_id=19c1f2d9474a551034c874bd436d43ff&sysparm_category=69bbcbde1b22c014503297539b4bcba0`;
 export const FORBIDDEN_ACTION = `https://sconnect.schaeffler.com/groups/cost-database-analytics/blog/2020/10/14/how-does-the-rights-management-works`;
