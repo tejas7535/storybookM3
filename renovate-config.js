bae3cadc79d5b3b3a14e3d57d69d227540c80676
Berndt, Christian  SF/HZA-ZC2C <berndcri@schaeffler.com>
2024-06-19 08:14:43 +0200

    ci(workspace): adjust postupgradetasks (#6299)

--- renovate-config.js
+++ renovate-config.js
@@ -3,8 +3,5 @@ module.exports = {
   repositories: ['Schaeffler-Group/frontend-schaeffler'],
   npmrc: 'registry=https://artifactory.schaeffler.com/artifactory/api/npm/npm/',
   allowPostUpgradeCommandTemplating: true,
-  allowedPostUpgradeCommands: [
-    'pnpm ci --ignore-scripts',
-    'pnpm run generate-readme',
-  ],
+  allowedPostUpgradeCommands: ['pnpm run generate-readme'],
 };
