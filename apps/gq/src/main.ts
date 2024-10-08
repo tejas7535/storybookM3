2ee3f991306db2b1252dbd1296d2c36f363b8ad9
Przybysz, Maciej  SW/WRO-DCDZC <przybmci@schaeffler.com>
2024-10-08 14:41:01 +0200

    chore(workspace): update ag grid license key (#6588)
    Co-authored-by: Poltorak, Krzysztof  SW/WRO-DCDZC <poltokzy@schaeffler.com>

--- apps/gq/src/main.ts
+++ apps/gq/src/main.ts
@@ -7,7 +7,7 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 LicenseManager.setLicenseKey(
-  `Using_this_AG_Grid_Enterprise_key_( AG-045886 )_in_excess_of_the_licence_granted_is_not_permitted___Please_report_misuse_to_( legal@ag-grid.com )___For_help_with_changing_this_key_please_contact_( info@ag-grid.com )___( Schaeffler Technologies AG & Co. KG )_is_granted_a_( Multiple Applications )_Developer_License_for_( 8 )_Front-End_JavaScript_developers___All_Front-End_JavaScript_developers_need_to_be_licensed_in_addition_to_the_ones_working_with_AG_Grid_Enterprise___This_key_has_not_been_granted_a_Deployment_License_Add-on___This_key_works_with_AG_Grid_Enterprise_versions_released_before_( 28 August 2024 )____[v2]_MTcyNDc5OTYwMDAwMA==80fd7d0000935fba2a7639b3f6169e76`
+  `Using_this_{AG_Grid}_Enterprise_key_{AG-068056}_in_excess_of_the_licence_granted_is_not_permitted___Please_report_misuse_to_legal@ag-grid.com___For_help_with_changing_this_key_please_contact_info@ag-grid.com___{Schaeffler_Technologies_AG_&_Co._KG}_is_granted_a_{Single_Application}_Developer_License_for_the_application_{Data_Science_Solutions}_only_for_{16}_Front-End_JavaScript_developers___All_Front-End_JavaScript_developers_working_on_{Data_Science_Solutions}_need_to_be_licensed___{Data_Science_Solutions}_has_not_been_granted_a_Deployment_License_Add-on___This_key_works_with_{AG_Grid}_Enterprise_versions_released_before_{2_November_2025}____[v3]_[01]_MTc2MjA0MTYwMDAwMA==90817029a626fd310e58ecd38ec534de`
 );
 
 if (environment.production) {
