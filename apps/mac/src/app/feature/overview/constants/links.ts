270c1d1d2c55ea39258d1e19a0e7e677eb221bf6
Konrad, Michael  SF/HZA-ZC2C <konramic@schaeffler.com>
2024-11-13 10:07:30 +0100

    fix(mac): replace s-connect links on overview page (UFTABI-8086) (#6698)

--- apps/mac/src/app/feature/overview/constants/links.ts
+++ apps/mac/src/app/feature/overview/constants/links.ts
@@ -1,34 +1,34 @@
 export const links: { link: string; text: string; externalLink?: boolean }[] = [
   {
-    link: 'https://sconnect.schaeffler.com/groups/material-app-center',
+    link: 'https://worksite.sharepoint.com/sites/Central-Technologies/SitePages/Materials-App-C.aspx?csf=1&web=1&e=OcV2pG&xsdata=MDV8MDJ8a3JhdXNyYmVAc2NoYWVmZmxlci5jb218MmZiY2Q2NDI0OTQ4NDMwYzYwYTMwOGRjZjFkODg4YWN8Njc0MTY2MDQ2NTA5NDAxNDk4NTk0NWU3MDlmNTNkM2Z8MXwwfDYzODY1MTE1OTUyMTI0MjA0N3xVbmtub3dufFRXRnBiR1pzYjNkOGV5SldJam9pTUM0d0xqQXdNREFpTENKUUlqb2lWMmx1TXpJaUxDSkJUaUk2SWsxaGFXd2lMQ0pYVkNJNk1uMD18MHx8fA%3d%3d&sdata=SkxxZzNPbnNzWjRSTS9JZ3lMVXZPSXpPUWJuaXhMQW54MWFaU1RMdDhyYz0%3d',
     text: 'materialsAppCenter',
   },
   {
-    link: 'https://sconnect.schaeffler.com/community/global-technology/rd-competence-services/corporate-materials',
+    link: 'https://worksite.sharepoint.com/sites/Central-Technologies/SitePages/de/Material-Technologies.aspx?xsdata=MDV8MDJ8a3JhdXNyYmVAc2NoYWVmZmxlci5jb218MmZiY2Q2NDI0OTQ4NDMwYzYwYTMwOGRjZjFkODg4YWN8Njc0MTY2MDQ2NTA5NDAxNDk4NTk0NWU3MDlmNTNkM2Z8MXwwfDYzODY1MTE1OTUyMTI1NTY3OHxVbmtub3dufFRXRnBiR1pzYjNkOGV5SldJam9pTUM0d0xqQXdNREFpTENKUUlqb2lWMmx1TXpJaUxDSkJUaUk2SWsxaGFXd2lMQ0pYVkNJNk1uMD18MHx8fA%3d%3d&sdata=VFpRR0hqbEt4YzdlcUdpU1NDN0FzMEVyTUNRQll4cDBlUzBzOXBPRWZ5dz0%3d',
     text: 'materials',
   },
   {
-    link: 'https://sconnect.schaeffler.com/groups/steel-cleanliness-database',
-    text: 'steelCleanlinessDataBase',
-  },
-  {
-    link: 'https://sconnect.schaeffler.com/docs/DOC-343113',
+    link: 'https://worksite.sharepoint.com/sites/Quality/SitePages/Product_Traceability.aspx?xsdata=MDV8MDJ8a3JhdXNyYmVAc2NoYWVmZmxlci5jb218MmZiY2Q2NDI0OTQ4NDMwYzYwYTMwOGRjZjFkODg4YWN8Njc0MTY2MDQ2NTA5NDAxNDk4NTk0NWU3MDlmNTNkM2Z8MXwwfDYzODY1MTE1OTUyMTE5MDM4MHxVbmtub3dufFRXRnBiR1pzYjNkOGV5SldJam9pTUM0d0xqQXdNREFpTENKUUlqb2lWMmx1TXpJaUxDSkJUaUk2SWsxaGFXd2lMQ0pYVkNJNk1uMD18MHx8fA%3d%3d&sdata=a3dqSVFWU3ZqVWtjaWQ5NEs5VkEyQXVZcUkwaHYvMWFNYXdQL1pZZTM2OD0%3d',
     text: 'globalBatchTraceability',
   },
   {
-    link: 'https://sconnect.schaeffler.com/docs/DOC-129499',
+    link: 'https://www.schaeffler.com/remotemedien/media/_shared_media/08_media_library/01_publications/schaeffler_2/tpi/downloads_8/tpi_226_de_de.pdf',
+    text: 'materialsForRollingBearingTechnologyDe',
+  },
+  {
+    link: 'https://www.schaeffler.com/remotemedien/media/_shared_media/08_media_library/01_publications/schaeffler_2/tpi/downloads_8/tpi_226_de_en.pdf',
+    text: 'materialsForRollingBearingTechnologyEn',
+  },
+  {
+    link: undefined,
+    text: 'steelCleanlinessDataBase',
+  },
+  {
+    link: undefined,
     text: 'estimationOfMaterialProperties',
   },
   {
-    link: 'https://sconnect.schaeffler.com/docs/DOC-26552',
-    text: 'materialsForRollingBearingTechnology',
-  },
-  {
-    link: 'https://sconnect.schaeffler.com/community/global-technology/rd-competence-services/corporate-materials/materials-technology/materials-development/blog/2019/04/09/schaeffler-materials-faq',
-    text: 'materialsFaq',
-  },
-  {
-    link: 'https://sconnect.schaeffler.com/docs/DOC-156511',
+    link: undefined,
     text: 'comparisonOfQuenchingMedia',
   },
   {
