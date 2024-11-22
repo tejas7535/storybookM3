5c123b4bc33d2f6d72cbeb117afa252db2044968
Hanft, Michael  (ext.)  SF/HZA-ZCQA <hanftmch@schaeffler.com>
2024-11-22 12:44:36 +0100

    fix(d360): set the correct tab after page refresh (SFT-1956) (#6751)

--- apps/d360/src/app/shared/components/page/tab-bar-navigation/tab-bar-navigation.component.ts
+++ apps/d360/src/app/shared/components/page/tab-bar-navigation/tab-bar-navigation.component.ts
@@ -1,9 +1,16 @@
-import { AsyncPipe } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import {
+  Component,
+  effect,
+  inject,
+  input,
+  InputSignal,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatTabsModule } from '@angular/material/tabs';
-import { Data, NavigationEnd, Router, RouterModule } from '@angular/router';
+import { Data, Router, RouterModule } from '@angular/router';
 
 import { Observable, of } from 'rxjs';
 
@@ -16,6 +23,12 @@ import { AuthService } from '../../../utils/auth/auth.service';
 
 type TabItem = 'start-page' | 'functions' | 'tasks';
 
+/**
+ * Component to handle the tab bar navigation in the application.
+ *
+ * @export
+ * @class TabBarNavigationComponent
+ */
 @Component({
   selector: 'd360-tab-bar-navigation',
   standalone: true,
@@ -25,51 +38,93 @@ type TabItem = 'start-page' | 'functions' | 'tasks';
     MatTabsModule,
     MatMenuModule,
     MatIconModule,
-    AsyncPipe,
     PushPipe,
   ],
   templateUrl: './tab-bar-navigation.component.html',
   styleUrl: './tab-bar-navigation.component.scss',
 })
-export class TabBarNavigationComponent implements OnInit {
+export class TabBarNavigationComponent {
   private readonly router = inject(Router);
   private readonly authService = inject(AuthService);
 
-  routeConfig = appRoutes;
+  /**
+   * Reference to route configuration
+   *
+   * @protected
+   * @memberof TabBarNavigationComponent
+   */
+  protected routeConfig = appRoutes;
 
-  activeTab: TabItem = 'start-page';
+  /**
+   * Signal to keep track of active tab
+   *
+   * @protected
+   * @type {WritableSignal<TabItem>}
+   * @memberof TabBarNavigationComponent
+   */
+  protected activeTab: WritableSignal<TabItem> = signal('start-page');
 
-  ngOnInit() {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        this.activeTab = this.getTabItemForRoute(event.url);
-      }
-    });
+  /**
+   * Input signal for the active URL
+   *
+   * @type {InputSignal<string>}
+   * @memberof TabBarNavigationComponent
+   */
+  public activeUrl: InputSignal<string> = input.required();
+
+  /**
+   * Creates an instance of TabBarNavigationComponent.
+   * The constructor sets up an effect to update the active tab based on the route changes.
+   *
+   * @memberof TabBarNavigationComponent
+   */
+  public constructor() {
+    effect(
+      () => this.activeTab.set(this.getTabItemForRoute(this.activeUrl())),
+      { allowSignalWrites: true }
+    );
   }
 
-  navigateTo(path: string) {
-    this.router.navigate([path]).then(() => {
-      this.activeTab = this.getTabItemForRoute(path);
-    });
+  /**
+   * Method to navigate to a specific path using the router service.
+   *
+   * @protected
+   * @param {string} path - The target URL for navigation.
+   * @memberof TabBarNavigationComponent
+   */
+  protected navigateTo(path: string) {
+    this.router.navigate([path]);
   }
 
-  getTabItemForRoute = (route: string): TabItem => {
-    if (route === '/' || route === '') {
+  /**
+   * Private method to determine the current tab item based on the given route.
+   *
+   * @private
+   * @param {string} route - The active URL for which the tab item needs to be identified.
+   * @returns {TabItem} - The corresponding tab item for the provided route.
+   * @memberof TabBarNavigationComponent
+   */
+  private getTabItemForRoute(route: string): TabItem {
+    if (['/', ''].includes(route)) {
       return 'start-page';
-    } else if (route === '/tasks' || route === 'tasks') {
+    } else if (['/tasks', 'tasks'].includes(route)) {
       return 'tasks';
     }
 
     return 'functions';
-  };
+  }
 
-  canNavigateTo$ = (data: Data): Observable<boolean> => {
-    if (!data || !data.allowedRoles) {
-      return of(true);
-    }
-
-    const allowedRoles = data.allowedRoles;
-
-    return this.authService.hasUserAccess(allowedRoles);
-  };
+  /**
+   * Method to check whether navigation is allowed based on the provided data.
+   *
+   * @protected
+   * @param {Data} data - The data object containing information about required access roles.
+   * @returns {Observable<boolean>} - An observable that emits a boolean value indicating the authorization status.
+   * @memberof TabBarNavigationComponent
+   */
+  protected canNavigateTo$(data: Data): Observable<boolean> {
+    return !data || !data.allowedRoles
+      ? of(true)
+      : this.authService.hasUserAccess(data.allowedRoles);
+  }
 }
