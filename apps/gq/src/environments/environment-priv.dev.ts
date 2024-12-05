5c46428561ad9ae65fbdcded5aea8349d4dd2434
Herpich, Stefan  SF/HZA-ZC3A <herpisef@schaeffler.com>
2024-12-05 13:53:42 +0100

    feat(gq): simultaneous CI for private and public cluster (GQUOTE-5272) (#6802)
    Co-authored-by: Soehnlein, Pascal  SF/HZA-ZC2A <soehnpsc@schaeffler.com>

--- apps/gq/src/environments/environment-priv.dev.ts
+++ apps/gq/src/environments/environment-priv.dev.ts
@@ -1,17 +1,14 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 import { EnvironmentEnum } from '../app/shared/models';
 import { Environment } from './environment.model';
 
 export const environment: Environment = {
   production: false,
-  localDev: true,
+  localDev: false,
   devToolsEnabled: true,
   clientId: '863a0742-f493-4499-bc30-9c81772fe96a',
   tenantId: '67416604-6509-4014-9859-45e709f53d3f',
   appScope: 'api://acea7ac1-13bb-44b3-be1e-76fc729472be/gqapi',
-  envName: 'https://guided-quoting-d.dev.dp.schaeffler (dev)',
+  envName: 'https://guided-quoting.de-d.aks.schaeffler.com (dev)',
   oneTrustId: '769f7370-251f-48bd-abe1-cf7907e49189',
   applicationInsights: {
     applicationInsightsConfig: {
@@ -27,12 +24,3 @@ export const environment: Environment = {
   },
   environment: EnvironmentEnum.dev,
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
